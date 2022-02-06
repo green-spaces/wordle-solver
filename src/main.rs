00515d21ec1e302d053d62e8e19aeaaa72cf41bf
Matthew Taylor <mwtaylor902@gmail.com>
2022-02-06 09:51:12 -0500

    Refactoring solver to work for multiple iterations

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,34 @@
 use std::fs::File;
 use std::io::{self, prelude::*};
 
-const WORD_SOURCE: &str = "less-englisth-words.txt";
+const WORD_SOURCE: &str = "scrabble.txt";
 
 fn main() {
-    let words = read_lines(WORD_SOURCE);
-    let mut best_word: String;
+    let dictionary = load_dictionary(WORD_SOURCE);
+    let mut candidates = dictionary.clone();
+
+    let guess = optimial_guess(&candidates, &dictionary);
+    println!("{} => {}", guess, worst_cast_words_left(&guess, &candidates));
+
+
+    println!("Five Letter Words: {}", dictionary.len());
+}
+
+fn optimial_guess(candidates: &Vec<String>, dictionary: &Vec<String>) -> String {
+    let mut best_word: String = dictionary[0].clone();
     let mut min_options = u32::MAX;
 
-    for word in words.iter() {
-        let res = remaining_words(word, &words);
+    for word in dictionary.iter() {
+        let res = worst_cast_words_left(word, &candidates);
         if res <= min_options {
             best_word = word.clone();
             min_options = res;
             println!("{} => {}", best_word, min_options);
         }
     }
-
-    println!("Five Letter Words: {}", words.len());
+    best_word
 }
+
 
 fn word_votes(word: &String, guess: &String) -> usize {
     let mut out = 0;
@@ -39,12 +49,12 @@ fn matching_votes(word: &String, word_list: &Vec<String>) -> Vec<u32> {
 }
 
 /// Returns the worst case remaining number of words
-fn remaining_words(word: &String, word_list: &Vec<String>) -> u32 {
+fn worst_cast_words_left(word: &String, word_list: &Vec<String>) -> u32 {
     let votes = matching_votes(word, word_list);
     *votes.iter().max().unwrap()
 }
 
-fn read_lines(file: &str) -> Vec<String> {
+fn load_dictionary(file: &str) -> Vec<String> {
     let file = File::open(file).unwrap();
     let lines = io::BufReader::new(file).lines();
 
