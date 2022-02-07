d315bd2b2196575407f9afdb10bc6b45850c819b
Matthew Taylor <mwtaylor902@gmail.com>
2022-02-06 23:28:42 -0500

    Mostly working process

--- src/main.rs
+++ src/main.rs
@@ -6,33 +6,77 @@ const WORD_SOURCE: &str = "scrabble.txt";
 fn main() {
     let dictionary = load_dictionary(WORD_SOURCE);
     let mut candidates = dictionary.clone();
-
-    let guess = optimial_guess(&candidates, &dictionary);
-    println!("{} => {}", guess, worst_cast_words_left(&guess, &candidates));
-
-
     println!("Five Letter Words: {}", dictionary.len());
+
+    while candidates.len() != 0 {
+        let guess = optimial_guess(&candidates, &dictionary);
+        println!(
+            "{} => {}",
+            guess,
+            worst_cast_words_left(&guess, &candidates)
+        );
+        let response = read_input();
+        candidates = prune_candidates(&guess, &response, &candidates);
+        println!("{:?}", candidates);
+    }
+}
+
+fn prune_candidates(guess: &String, response: &String, candidates: &Vec<String>) -> Vec<String> {
+    let mut new_candidates = Vec::new();
+    for candidate in candidates {
+        let mut keep = true;
+
+        /// Check direct matches
+        for ((g, r), c) in guess.chars().zip(response.chars()).zip(candidate.chars()) {
+            match r {
+                'g' => {
+                    keep &= g == c;
+                }
+                'b' => {
+                    keep &= !candidate.chars().any(|c| c == g);
+                }
+                'y' => {
+                    keep &= candidate.chars().any(|c| c == g);
+                }
+                _ => {}
+            }
+        }
+
+        if keep {
+            new_candidates.push(candidate.clone())
+        }
+    }
+
+    new_candidates
+}
+
+fn read_input() -> String {
+    let mut buffer = String::new();
+    io::stdin().read_line(&mut buffer).unwrap();
+    buffer
 }
 
 fn optimial_guess(candidates: &Vec<String>, dictionary: &Vec<String>) -> String {
+    if candidates.len() == 1 {
+        return candidates[0].clone();
+    }
+    
     let mut best_word: String = dictionary[0].clone();
     let mut min_options = u32::MAX;
 
     for word in dictionary.iter() {
         let res = worst_cast_words_left(word, &candidates);
-        if res <= min_options {
+        if res < min_options {
             best_word = word.clone();
             min_options = res;
-            println!("{} => {}", best_word, min_options);
         }
     }
     best_word
 }
 
-
 fn word_votes(word: &String, guess: &String) -> usize {
     let mut out = 0;
-    for (idx, w) in word.chars().enumerate(){
+    for (idx, w) in word.chars().enumerate() {
         if guess.chars().any(|g| g == w) {
             out += usize::pow(2, idx.try_into().unwrap());
         }
