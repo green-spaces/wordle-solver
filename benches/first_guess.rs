b35dd3f31781c2bdd72a7bb3015e4971df15f7bc
Matthew Taylor <mwtaylor902@gmail.com>
2022-02-08 09:07:41 -0500

    reduced benchmark sample size to 10

--- benches/first_guess.rs
+++ benches/first_guess.rs
@@ -1,3 +1,6 @@
+#[macro_use]
+extern crate lazy_static;
+
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
 
 use wordle_solver::guess_generator::optimial_guess;
@@ -5,15 +8,23 @@ use wordle_solver::io::load_dictionary;
 
 const WORD_SOURCE: &str = "less-less-english-words.txt";
 
+lazy_static! {
+    static ref DICTIONARY: Vec<String> = load_dictionary(WORD_SOURCE);
+}
+
 fn time_to_first_guess(i: i32) -> i32 {
-    let dictionary = load_dictionary(WORD_SOURCE);
-    let candidates = dictionary.clone();
-    let guess_full = optimial_guess(&candidates, &dictionary);
+    let candidates = DICTIONARY.clone();
+    let guess_full = optimial_guess(&candidates, &DICTIONARY);
     i * 2
 }
 
 fn criterion_benchmark(c: &mut Criterion) {
-    c.bench_function("outcome", |b| b.iter(|| time_to_first_guess(black_box(2))));
+    let mut group = c.benchmark_group("sample-size-example");
+    // Configure Criterion.rs to detect smaller differences and increase sample size to improve
+    // precision and counteract the resulting noise.
+    group.significance_level(0.1).sample_size(10);
+    group.bench_function("outcome", |b| b.iter(|| time_to_first_guess(black_box(2))));
+    group.finish();
 }
 
 criterion_group!(benches, criterion_benchmark);
