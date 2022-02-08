ffc4167a74043f26cbd30c6fa37243ddd8ae7aa5
Matthew Taylor <mwtaylor902@gmail.com>
2022-02-08 01:24:26 -0500

    Added both of the options

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use wordle_solver::guess_result::{self, GuessResult};
 use wordle_solver::io::{load_dictionary, read_wordle_output};
 use wordle_solver::rank::outcome;
 
@@ -11,56 +12,32 @@ fn main() {
     println!("Five Letter Words: {}", dictionary.len());
 
     while !candidates.is_empty() {
-        let guess = optimial_guess(&candidates, &dictionary);
-        println!("{} => {}", guess, entropy_votes(&guess, &candidates));
+        let guess_full = optimial_guess(&candidates, &dictionary);
+        let guess_constrained = optimial_guess(&candidates, &candidates);
+        println!(
+            "Full: {} => {}",
+            guess_full,
+            entropy_votes(&guess_full, &candidates)
+        );
+        println!(
+            "Remaining: {} => {}",
+            guess_constrained,
+            entropy_votes(&guess_constrained, &candidates)
+        );
         let response = read_wordle_output();
-        candidates = prune_candidates(&guess, response.into_inner(), &candidates);
-        // println!("{:?}", candidates);
+        candidates = prune_candidates(&guess_full, &response, &candidates);
+        // println!("Remaining words: {}", candidates.len());
     }
 }
 
-fn prune_candidates(guess: &str, response: &str, candidates: &[String]) -> Vec<String> {
-    let mut new_candidates = Vec::new();
-    for candidate in candidates {
-        let mut keep = true;
+fn prune_candidates(guess: &str, guess_result: &GuessResult, candidates: &[String]) -> Vec<String> {
+    let guess_score = guess_result.score();
 
-        // Check direct matches
-        for ((idx_g, (g, r)), (idx_c, c)) in guess
-            .chars()
-            .zip(response.chars())
-            .enumerate()
-            .zip(candidate.chars().enumerate())
-        {
-            match r {
-                'g' => {
-                    keep &= g == c;
-                }
-                'y' => {
-                    keep &= candidate.chars().any(|c| c == g)
-                        && guess.chars().nth(idx_g) != candidate.chars().nth(idx_c);
-                }
-                'b' => {
-                    // Needs a special case for words with two letters
-                    let c_count = candidate.matches(g).count();
-                    let g_count = guess.matches(g).count();
-
-                    keep &= c_count < g_count;
-                }
-                _ => {}
-            }
-        }
-
-        // Dont keep incorrect guesses
-        if candidate == guess {
-            keep = false;
-        }
-
-        if keep {
-            new_candidates.push(candidate.clone())
-        }
-    }
-
-    new_candidates
+    candidates
+        .iter()
+        .filter(|can| outcome(guess, *can).score() == guess_score)
+        .map(|s| s.clone())
+        .collect::<Vec<String>>()
 }
 
 fn optimial_guess(candidates: &[String], dictionary: &[String]) -> String {
@@ -77,10 +54,15 @@ fn optimial_guess(candidates: &[String], dictionary: &[String]) -> String {
     best_word
 }
 
+/// Returns the worst case remaining number of words
+fn entropy_votes(word: &str, word_list: &[String]) -> f32 {
+    let votes = matching_votes(word, word_list);
+    entropy(&votes)
+}
+
 fn matching_votes(word: &str, word_list: &[String]) -> Vec<usize> {
     let mut votes: Vec<usize> = vec![0; usize::pow(3, word.len().try_into().unwrap())];
     for guess in word_list {
-
         votes[outcome(word, guess).score()] += 1;
     }
     votes
@@ -93,10 +75,4 @@ fn entropy(votes: &[usize]) -> f32 {
         .iter()
         .map(|v| -(*v as f32) / count * f32::ln((*v as f32 + 0.01) / count))
         .sum::<f32>()
-}
-
-/// Returns the worst case remaining number of words
-fn entropy_votes(word: &str, word_list: &[String]) -> f32 {
-    let votes = matching_votes(word, word_list);
-    entropy(&votes)
 }
