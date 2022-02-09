cd364457890e6a303a9ba98650943f4f33922ea5
Matthew Taylor <mwtaylor902@gmail.com>
2022-02-08 23:48:25 -0500

    Parallelized the main for loop over the dictionary of words

--- src/guess_generator.rs
+++ src/guess_generator.rs
@@ -1,19 +1,43 @@
+use rayon::prelude::*;
 use super::rank::outcome_score;
 
 pub fn optimial_guess<'a>(candidates: &[String], dictionary: &'a [String]) -> &'a String {
     let mut best_word: &String = &dictionary[0];
     let mut max_entropy = f32::MIN;
 
-    let mut votes: Vec<f32> = vec![0.0; usize::pow(3, candidates[0].len().try_into().unwrap())];
+    let entropies: Vec<f32> = dictionary.into_par_iter().map(|word| {
+        entropy_votes_p(word, candidates)
+    }).collect();
 
-    for word in dictionary.iter() {
-        let res = entropy_votes(word, candidates, &mut votes);
-        if res > max_entropy || (res == max_entropy && candidates.iter().any(|cad| cad == word)) {
-            best_word = &word;
-            max_entropy = res;
+    for (&entropy, word) in entropies.iter().zip(dictionary) {
+        if entropy > max_entropy || (entropy == max_entropy && candidates.iter().any(|cad| cad == word)) {
+            best_word = word;
+            max_entropy = entropy;
         }
-    }
+    } 
+    
     best_word
+}
+
+pub fn entropy_votes_p(word: &str, word_list: &[String]) -> f32 {
+    entropy_p(&matching_votes_p(word, word_list))
+}
+
+fn matching_votes_p(word: &str, word_list: &[String]) -> Vec<f32> {
+    let mut votes: Vec<f32> = vec![0.0; usize::pow(3, 5)];
+
+    for guess in word_list {
+        votes[outcome_score(word, guess)] += 1.0;
+    };
+    votes
+}
+
+fn entropy_p(votes: &[f32]) -> f32 {
+    let count: f32 = votes.iter().sum();
+    votes
+        .iter()
+        .map(|v| -*v / count * (f32::ln(*v / count + 0.001)))
+        .sum::<f32>()
 }
 
 /// Returns the worst case remaining number of words
